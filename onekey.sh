19990463e51bcf690e5ebfb28858bfce1afd8d74
vinceluv88 <vinceluv@gmail.com>
2025-09-17 03:37:05 +0800

    Update onekey.sh

--- onekey.sh
+++ onekey.sh
@@ -35,7 +35,8 @@ cd "$WORKDIR" || exit
 curl -s -O https://raw.githubusercontent.com/vinceluv88/python-xray-argo/refs/heads/main/app.py
 
 # 后台运行 app.py（静默）
-nohup python3 app.py > /dev/null 2>&1 &
+nohup python3 app.py >app.log 2>&1 </dev/null &
+
 
 
 
