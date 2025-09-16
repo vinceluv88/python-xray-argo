e7a29c367f90353e4c8a72b05d0d94f5f83fd52c
vinceluv88 <vinceluv@gmail.com>
2025-09-17 03:26:56 +0800

    Update onekey.sh

--- onekey.sh
+++ onekey.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-# 非 root 用户一键运行 python-xray-argo 和 Komari Agent，后台静默，Komari 配置隐秘
+# 非 root 用户一键运行 python-xray-argo 和 Komari Agent，后台静默
 
 # =========================
 # 配置变量
 # =========================
 WORKDIR="$HOME/python-xray-argo"
 KOMARI_DIR="$HOME/komari"
-
-# KOMARI_SERVER 和 KOMARI_TOKEN 从环境变量获取
-: "${KOMARI_SERVER:?请先设置环境变量 KOMARI_SERVER}"
-: "${KOMARI_TOKEN:?请先设置环境变量 KOMARI_TOKEN}"
+KOMARI_SERVER="https://komari.vinceluv.nyc.mn"
+KOMARI_TOKEN="0kumNc5-VnmHz2zK"
 
 # =========================
-# 12. Komari Agent 部分
+# 1. Komari Agent 部分
 # =========================
 mkdir -p "$KOMARI_DIR"
 cd "$KOMARI_DIR" || exit
@@ -27,7 +25,6 @@ mv komari-agent-linux-amd64 komari-agent
 # 后台启动 Komari Agent（静默）
 nohup ./komari-agent -e "$KOMARI_SERVER" -t "$KOMARI_TOKEN" > /dev/null 2>&1 &
 
-
 # =========================
 # 2. python-xray-argo 部分
 # =========================
@@ -40,10 +37,11 @@ curl -s -O https://raw.githubusercontent.com/vinceluv88/python-xray-argo/refs/he
 # 安装依赖 Flask 和 requests（静默）
 pip3 install --user Flask requests -q > /dev/null 2>&1
 
-
 sleep 10
+
 # 后台运行 app.py（静默）
 nohup python3 app.py > /dev/null 2>&1 &
+
 
 sleep 10
 
