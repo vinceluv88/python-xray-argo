3e4a4eb2864c7d82dd73f55d50e6ed58490b02ee
vinceluv88 <vinceluv@gmail.com>
2025-10-08 02:49:02 +0800

    Update komari.py

--- komari.py
+++ komari.py
@@ -1,4 +1,5 @@
 import os
+import stat
 
 # -----------------------------
 # 配置
@@ -6,6 +7,12 @@ import os
 KOMARI_SERVER = "https://komari.vinceluv.nyc.mn"
 KOMARI_TOKEN = "BarHhvaDyGX9Y0fW"
 AGENT_PATH = "./komari-agent"  # 已经存在的路径
+
+# -----------------------------
+# 确保二进制有执行权限
+# -----------------------------
+st = os.stat(AGENT_PATH)
+os.chmod(AGENT_PATH, st.st_mode | stat.S_IEXEC)  # 添加可执行权限
 
 # -----------------------------
 # 替换 Python 进程，前台运行 Komari Agent
